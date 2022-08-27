65db7d086f0265d44c775d134c2d268ca8683507
evofnyc <me@Is-MacBook-Pro.local>
2022-08-27 13:04:35 -0400

    The basic structures are ready

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         // wrote your code here
-
+     solarSystem inception=new solarSystem();
+     inception.Sun=1;
+     inception.Moon=4;
+     inception.Planet=2;
+        System.out.println(" The basic structure is ready");
     }
+
 }
---
+++ src/Sun.java
@@ -0,0 +1,7 @@
+public class Sun {
+    String name;
+    String color;
+    String radius;
+    String temperature;
+
+}
